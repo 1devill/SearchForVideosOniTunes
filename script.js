40d53fdeee7e05126c38783a822027d812c437c4
Daniil <dannydeceiver@gmail.com>
2019-10-13 19:45:59 +0300

    6th commit finally done

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let $searchForm = $("#search-form");
 let $videoList = $('#video-list');
 let $carouselInner = $('#carousel-inner');
 let videos = [];
-// let $carouselItem = $(".carousel-item");
-// console.log($carouselItem);
 
 $searchForm.on("submit", function(event) {
     event.preventDefault();
@@ -27,7 +25,6 @@ function getVideos(query) {
         console.log(response);
         videos = response.results;
         console.log(videos);
-        // console.log(response.resultCount);
         addVideos(videos);
     }).fail((error) => {
         console.log(error);
@@ -41,40 +38,24 @@ function addVideos(data) {
         $("<div>").addClass("carousel-item")
             .appendTo($carouselInner);
     });
-    $("<video>")
-            .attr("src", data[0].previewUrl)
-            .appendTo(".carousel-item");
-    // for (let i = 0; i < data.length; i++) {
-    //     if ($(".carousel-item:has(video)")) {
-    //         console.log("nothing");   
-    //     } 
-    //         $("<video>")
-    //             .attr("src", data[i].previewUrl)
-    //             .appendTo(".carousel-item");
-    // }
+    let $carouselItem = $(".carousel-item");
+    data.forEach((item, index) => {
+        $("<video>")
+            .attr("src", data[index].previewUrl)
+            .attr("controls", true)
+            .width("100%")
+            .height("350px")
+            .appendTo($carouselItem[index]);
+    });
+    $(".carousel-item:first-child").addClass("active");
 }
 
-// $carouselInner.on("click", "[data-id]", function(event) {
-//     event.preventDefault();
+let $left = $(".carousel-control-prev");
+$left.on("click", function (){
+    $('video').trigger('pause');
+});
 
-//     let bookId = $(this).data("id");
-
-//     let book = books.find((item) => item.id === bookId);
-//     $currentBook.fadeIn();
-
-//     $currentBook.find(".book-title").text(`${book.volumeInfo.title} 
-//     | ${book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "No author"}
-//     ${book.volumeInfo.publishedDate}`)
-
-//     $bookDescription.empty();
-//     $("<img>").attr("src", book.volumeInfo.imageLinks.thumbnail)
-//         .appendTo($bookDescription);
-
-//     $("<p>").text(book.volumeInfo.description).appendTo($bookDescription);
-
-//     $("<a>").attr("href", book.volumeInfo.previewLink)
-//         .attr("target", "_blank")
-//         .text("Read more...")
-//         .addClass("read-link")
-//         .appendTo($bookDescription);
-// });
+let $right = $(".carousel-control-next");
+$right.on("click", function (){
+    $('video').trigger('pause');
+});
